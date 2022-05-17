b53b35ff4ca6affc87502f5b4b1da430f8a4385d
Salnik Valeriia <melfed.salnik@gmail.com>
2022-05-17 21:06:28 +0200

    Initial Commit

---
+++ SliderAppSwiftUI/SliderAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAppSwiftUIApp.swift
+//  SliderAppSwiftUI
+//
+//  Created by Валерия Смецкая on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
