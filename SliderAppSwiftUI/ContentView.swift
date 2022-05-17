edfebfddba34ada69da5b011de356cc2a0731408
Salnik Valeriia <melfed.salnik@gmail.com>
2022-05-17 22:01:16 +0200

    Set up user interface

--- SliderAppSwiftUI/ContentView.swift
+++ SliderAppSwiftUI/ContentView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var red: Double = 0
+    @State private var green: Double = 190
+    @State private var blue: Double = 255
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(white: 0.9)
+                .ignoresSafeArea()
+            VStack {
+                ColorView(red: red, green: green, blue: blue)
+                
+                SliderView(sliderValue: $red, textColor: .red)
+                SliderView(sliderValue: $green, textColor: .green)
+                SliderView(sliderValue: $blue, textColor: .blue)
+                
+                Spacer()
+            }.padding()
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
